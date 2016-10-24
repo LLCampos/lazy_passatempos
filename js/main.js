b1f26dbd45498edcdd0868a4ccf6b46609529317
LLCampos <luis1212@sapo.pt>
2016-10-24 18:05:39 +0100

    big refactor. start using airbnb style guide.

--- js/main.js
+++ js/main.js
@@ -1,98 +1,96 @@
-$().ready(function($) {
+/* jshint esversion: 6 */
 
-    $("#passatempos").on('click', 'ul li', function() {
-        $(this).toggleClass('checked');
-    });
+const passatempos = {
 
-    if (passatempos.isStored()) {
-        passatempos.showPassatempos();
-    } else {
-        passatempos.getNewPassatempos();
+  crawler_server_domain: 'http://localhost:5001/',
+
+  isStored() {
+    if (localStorage.passatempos) {
+      return true;
     }
+    return false;
+  },
 
-});
+  getLocalPassatempos() {
+    return JSON.parse(localStorage.passatempos);
+  },
 
-
-var getLastUpdate = function() {
-    var url = pcrawler_domain + 'last_update';
-
-    $.get(url, function(data) {
-        return data;
-    });
-};
-
-
-var passatempos = {
-
-    crawler_server_domain: 'http://localhost:5001/',
-
-    isStored: function() {
-        if (localStorage.passatempos) {
-            return true;
-        }
-        return false;
-    },
-
-    getLocalPassatempos: function() {
-        return JSON.parse(localStorage.passatempos);
-    },
-
-    setLocalPassatempos: function(passatempos_obj) {
-
-        if (!passatempos.isStored) {
-            for (var website in passatempos_obj) {
-                for (var i in passatempos_obj[website]) {
-                    passatempos_obj[website][i].checked = false;
-                }
-            }
-        }
-
-        passatempos_str = JSON.stringify(passatempos_obj);
-        localStorage.setItem("passatempos", passatempos_str);
-    },
-
-    getNewPassatempos: function() {
-        var url = this.crawler_server_domain + 'passatempos';
-
-        $.get(url, function(passatempos_obj) {
-            passatempos.setLocalPassatempos(passatempos_obj);
-            passatempos.showPassatempos();
+  setLocalPassatempos(passatemposObj) {
+    if (!passatempos.isStored) {
+      Object.keys(passatemposObj).forEach((website) => {
+        passatemposObj[website].forEach((obj, i) => {
+          passatemposObj[website][i].checked = false;
         });
-    },
-
-    showPassatempos: function() {
-
-        passatempos_obj = passatempos.getLocalPassatempos();
-
-        for (var website_name in passatempos_obj) {
-
-            passatempo_elem = $('<div class="passatempo">');
-            passatempo_elem.append('<h2>' + website_name + '</h2>');
-
-
-            var ul = $('<ul class="passatempos-list"></ul>');
-
-            for (var i in passatempos_obj[website_name]) {
-                var passatempo_obj =  passatempos_obj[website_name][i];
-
-                var li = $('<li>');
-
-                if (passatempo_obj.checked) {
-                    li.addClass('checked');
-                }
-
-                li.append('<a href="' + passatempo_obj.url + '">' + passatempo_obj.name + '</a>');
-                ul.append(li);
-            }
-
-            passatempo_elem.append(ul);
-            $('#passatempos').append(passatempo_elem);
-        }
-    },
-
-    checkPassatempo: function(website, index) {
-        passatempos_obj = passatempos.getLocalPassatempos();
-        passatempos_obj[website][index].checked = true;
-        passatempos.setLocalPassatempos(passatempos_obj);
+      });
     }
+
+    const passatemposStr = JSON.stringify(passatemposObj);
+    localStorage.setItem('passatempos', passatemposStr);
+  },
+
+  getNewPassatempos() {
+    const url = `${this.crawler_server_domain} passatempos`;
+
+    $.get(url, (passatemposObj) => {
+      passatempos.setLocalPassatempos(passatemposObj);
+      passatempos.showPassatempos();
+    });
+  },
+
+  showPassatempos() {
+    const passatemposObj = passatempos.getLocalPassatempos();
+
+    Object.keys(passatemposObj).forEach((websiteName) => {
+      const passatempoElem = $('<div class="passatempo">');
+      passatempoElem.append(`<h2>${websiteName}</h2>`);
+
+      const ul = $('<ul class="passatempos-list"></ul>');
+
+      passatemposObj[websiteName].forEach((obj, i) => {
+        const passatempoObj = passatemposObj[websiteName][i];
+
+        const li = $('<li>');
+        if (passatempoObj.checked) {
+          li.addClass('checked');
+        }
+
+        li.append(`<a href="${passatempoObj.url}">${passatempoObj.name}</a>`);
+        ul.append(li);
+      });
+
+      passatempoElem.append(ul);
+      $('#passatempos').append(passatempoElem);
+    });
+  },
+
+  checkPassatempo(website, index) {
+    const passatemposObj = passatempos.getLocalPassatempos();
+    passatemposObj[website][index].checked = true;
+    passatempos.setLocalPassatempos(passatemposObj);
+  },
+
+  uncheckPassatempo(website, index) {
+    const passatemposObj = passatempos.getLocalPassatempos();
+    passatemposObj[website][index].checked = false;
+    passatempos.setLocalPassatempos(passatemposObj);
+  },
+
+  getLastUpdate() {
+    const url = `${passatempos.crawler_server_domain}last_update`;
+    $.get(url, data => data);
+  },
+
 };
+
+
+$().ready(($) => {
+  $('#passatempos').on('click', 'ul li', function toogleChecked() {
+    $(this).toggleClass('checked');
+  });
+
+  if (passatempos.isStored()) {
+    passatempos.showPassatempos();
+  } else {
+    passatempos.getNewPassatempos();
+  }
+});
